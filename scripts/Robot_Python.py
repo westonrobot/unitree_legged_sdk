9beb6af4b396f9bc2f2b09e5fa068ffc142a02fe
Hongbo Zhu <49983377+DonovanZhu@users.noreply.github.com>
2021-11-12 17:12:20 +0800

    Update Robot_Python.py

--- scripts/Robot_Python.py
+++ scripts/Robot_Python.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 import Unitree_Python_sdk
-
+import time
 unitree_robot = Unitree_Python_sdk.Unitree_Robot()
 
+motion_time = 0
 while True:
-
+    time.sleep(0.002)
     # Uncomment the following line to control the pose of robot
     # The four arguments are: roll, pitch, yaw, bodyHeight.
      
@@ -19,9 +20,22 @@ while True:
     # Speed level: 0. default low speed. 1. medium speed 
     # Body height: unit: m, default: 0.28m
     
-    # state = unitree_robot.robot_walking(gaitType = 1, forwardSpeed = -0.1, sidewaySpeed = 0.0, rotateSpeed = 0.0, speedLevel = 0, bodyHeight = 0.0)
+    #state = unitree_robot.robot_walking(gaitType = 1, forwardSpeed = -0.1, sidewaySpeed = 0.0, rotateSpeed = 0.0, speedLevel = 0, bodyHeight = 0.0)
+
+    motion_time += 1
+    if (motion_time < 1000):
+        state = unitree_robot.robot_dance(1)
+    if (motion_time >= 1000 and motion_time < 2000):
+        state = unitree_robot.quit_dance()
+    if (motion_time >= 2000):
+        state = unitree_robot.robot_walking(gaitType = 1, forwardSpeed = 0.1, sidewaySpeed = 0.0, rotateSpeed = 0.0, speedLevel = 0, bodyHeight = 0.0)
+
+    
+        
     
     
+
+
     # return state of robot    
     # imu                       //rpy[0], rpy[1], rpy[3]
     # gaitType                  // 0.idle  1.trot  2.trot running  3.climb stair
